e3d40129084726db80e2a840fb0fe20fb9b59ddc
Patrik Król <patrik.krol@protonmail.ch>
2024-06-02 16:57:03 +0200

    Merge remote-tracking branch 'bytecoin/master'

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -135,7 +135,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "185.3.158.49:15050",
-  //"your_seed_ip2.com:8080",
+  "185.3.158.50:15050",
 };
 
 struct CheckpointData {
